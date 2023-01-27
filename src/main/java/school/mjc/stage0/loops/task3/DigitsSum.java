a853058d114ffee6809ba1a3ac7c9996912a3a35
Илья <ilyich223@gmail.com>
2023-01-27 16:22:34 +0200

    Tasks complete

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0;
+        while(t > 0) {
+            int digit = t % 10;
+            sum += digit;
+            t /= 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        int num = 9;
+        int i = 1;
+        while (i <= lengthOfLastNumber) {
+            sum += num;
+            num = num * 10 + 9;
+            i++;
+        }
+        System.out.println(sum);
     }
 }
